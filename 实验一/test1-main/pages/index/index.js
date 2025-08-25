76bb1689d26f24a687b4fc64582c77e0d52eb2d7
Chenmomo <chenyibing@stu.ouc.edu.cn>
2025-08-25 20:11:27 +0800

    增加所有实验一的代码以及实验一的实验报告
    实验一：设计一个【获取用户信息】的简单页面

---
+++ 实验一/test1-main/pages/index/index.js
@@ -0,0 +1,86 @@
+//index.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    src: '/images/logo.jpg',
+    name: 'Hello world',
+    hasUserInfo: false,
+    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
+    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+  },
+  //获取用户信息
+  getMyInfo: function(e) {
+    wx.getUserProfile({
+      desc: '展示用户信息',
+      success: (res) => {
+        console.log('用户信息获取成功', res)
+        this.setData({
+          name: res.userInfo.nickName, // 修正：从 userInfo 中获取昵称
+          src: res.userInfo.avatarUrl,     // 修正：从 userInfo 中获取头像
+          hasUserInfo: true
+        })
+      },
+      fail: (res) => {
+        console.log('用户信息获取失败', res)
+      },
+      complete: (res) => {},
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+})
